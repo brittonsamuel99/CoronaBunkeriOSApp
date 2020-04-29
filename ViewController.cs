6ae23e9be54e312a0accc9e36260139b53740670
Sam Britton <48329862+dgr8sambino@users.noreply.github.com>
2020-04-29 10:12:19 -0600

    Add files via upload

---
+++ ViewController.cs
@@ -0,0 +1,85 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace CoronaBunker
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void sheetsPerRoll_Changed(UISlider sender)
+        {
+            var temp = Convert.ToUInt32(sheetsPerRoll.Value);
+            sheetsPerRollNum.Text = temp.ToString();
+            Updater();
+        }
+
+        partial void rollsInStorage_Changed(UISlider sender)
+        {
+            var temp = Convert.ToInt32(rollsInStorage.Value);
+            rollsInStorageNum.Text = temp.ToString();
+            Updater();
+        }
+
+        partial void visitsPerDay_Changed(UISlider sender)
+        {
+            var temp = Convert.ToInt32(visitsPerDay.Value);
+            visitsPerDayNum.Text = temp.ToString();
+            Updater();
+        }
+
+        partial void sheetsPerWipe_Changed(UISlider sender)
+        {
+            var temp = Convert.ToInt32(sheetsPerWipe.Value);
+            sheetsPerWipeNum.Text = temp.ToString();
+            Updater();
+        }
+
+        partial void wipesPerTrip_Changed(UISlider sender)
+        {
+            var temp = Convert.ToInt32(wipesPerTrip.Value);
+            wipesPerTripNum.Text = temp.ToString();
+            Updater();
+        }
+
+        partial void peopleInHome_Changed(UISlider sender)
+        {
+            var temp = Convert.ToInt32(peopleInHome.Value);
+            peopleInHomeNum.Text = temp.ToString();
+            Updater();
+        }
+
+        void Updater()
+        {
+            var spr = Convert.ToUInt32(sheetsPerRoll.Value);
+            var ris = Convert.ToInt32(rollsInStorage.Value);
+            var vpd = Convert.ToInt32(visitsPerDay.Value);
+            var spw = Convert.ToInt32(sheetsPerWipe.Value);
+            var wpt = Convert.ToInt32(wipesPerTrip.Value);
+            var pih = Convert.ToInt32(peopleInHome.Value);
+
+            var sheetsPerDay = vpd * spw * wpt * pih;
+            var sheetsInStorage = spr * ris;
+
+            var days = sheetsInStorage / sheetsPerDay;
+
+            numDays.Text = days.ToString();
+        }
+
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,215 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace CoronaBunker
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel numDays { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider peopleInHome { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel peopleInHomeLbl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel peopleInHomeNum { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider rollsInStorage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel rollsInStorageLbl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel rollsInStorageNum { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sheetsPerRoll { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel sheetsPerRollLbl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel sheetsPerRollNum { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sheetsPerWipe { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel sheetsPerWipeLbl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel sheetsPerWipeNum { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider visitsPerDay { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel visitsPerDayLbl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel visitsPerDayNum { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider wipesPerTrip { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel wipesPerTripLbl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel wipesPerTripNum { get; set; }
+
+        [Action ("peopleInHome_Changed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void peopleInHome_Changed (UIKit.UISlider sender);
+
+        [Action ("rollsInStorage_Changed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void rollsInStorage_Changed (UIKit.UISlider sender);
+
+        [Action ("sheetsPerRoll_Changed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void sheetsPerRoll_Changed (UIKit.UISlider sender);
+
+        [Action ("sheetsPerWipe_Changed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void sheetsPerWipe_Changed (UIKit.UISlider sender);
+
+        [Action ("visitsPerDay_Changed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void visitsPerDay_Changed (UIKit.UISlider sender);
+
+        [Action ("wipesPerTrip_Changed:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void wipesPerTrip_Changed (UIKit.UISlider sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (numDays != null) {
+                numDays.Dispose ();
+                numDays = null;
+            }
+
+            if (peopleInHome != null) {
+                peopleInHome.Dispose ();
+                peopleInHome = null;
+            }
+
+            if (peopleInHomeLbl != null) {
+                peopleInHomeLbl.Dispose ();
+                peopleInHomeLbl = null;
+            }
+
+            if (peopleInHomeNum != null) {
+                peopleInHomeNum.Dispose ();
+                peopleInHomeNum = null;
+            }
+
+            if (rollsInStorage != null) {
+                rollsInStorage.Dispose ();
+                rollsInStorage = null;
+            }
+
+            if (rollsInStorageLbl != null) {
+                rollsInStorageLbl.Dispose ();
+                rollsInStorageLbl = null;
+            }
+
+            if (rollsInStorageNum != null) {
+                rollsInStorageNum.Dispose ();
+                rollsInStorageNum = null;
+            }
+
+            if (sheetsPerRoll != null) {
+                sheetsPerRoll.Dispose ();
+                sheetsPerRoll = null;
+            }
+
+            if (sheetsPerRollLbl != null) {
+                sheetsPerRollLbl.Dispose ();
+                sheetsPerRollLbl = null;
+            }
+
+            if (sheetsPerRollNum != null) {
+                sheetsPerRollNum.Dispose ();
+                sheetsPerRollNum = null;
+            }
+
+            if (sheetsPerWipe != null) {
+                sheetsPerWipe.Dispose ();
+                sheetsPerWipe = null;
+            }
+
+            if (sheetsPerWipeLbl != null) {
+                sheetsPerWipeLbl.Dispose ();
+                sheetsPerWipeLbl = null;
+            }
+
+            if (sheetsPerWipeNum != null) {
+                sheetsPerWipeNum.Dispose ();
+                sheetsPerWipeNum = null;
+            }
+
+            if (visitsPerDay != null) {
+                visitsPerDay.Dispose ();
+                visitsPerDay = null;
+            }
+
+            if (visitsPerDayLbl != null) {
+                visitsPerDayLbl.Dispose ();
+                visitsPerDayLbl = null;
+            }
+
+            if (visitsPerDayNum != null) {
+                visitsPerDayNum.Dispose ();
+                visitsPerDayNum = null;
+            }
+
+            if (wipesPerTrip != null) {
+                wipesPerTrip.Dispose ();
+                wipesPerTrip = null;
+            }
+
+            if (wipesPerTripLbl != null) {
+                wipesPerTripLbl.Dispose ();
+                wipesPerTripLbl = null;
+            }
+
+            if (wipesPerTripNum != null) {
+                wipesPerTripNum.Dispose ();
+                wipesPerTripNum = null;
+            }
+        }
+    }
+}
